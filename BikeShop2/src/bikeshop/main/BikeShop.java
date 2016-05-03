26135ccb506cbf7dc76b37546d2e3723e5685541
Gogs <gogs@fake.local>
2016-05-03 17:26:40 +0900

    커밋

---
+++ BikeShop2/src/bikeshop/main/BikeShop.java
@@ -0,0 +1,82 @@
+package bikeshop.main;
+
+public class BikeShop {
+	private int rentalCode;
+	private String rentalName;
+	private String rentalAdd;
+	private int ea;
+	private String Photo;
+	private String posX;
+	private String posY;
+	private float fee;
+	private String modelType;
+	private String ClassCode;
+	private String Areacode;
+	public int getRentalCode() {
+		return rentalCode;
+	}
+	public void setRentalCode(int rentalCode) {
+		this.rentalCode = rentalCode;
+	}
+	public String getRentalName() {
+		return rentalName;
+	}
+	public void setRentalName(String rentalName) {
+		this.rentalName = rentalName;
+	}
+	public String getRentalAdd() {
+		return rentalAdd;
+	}
+	public void setRentalAdd(String rentalAdd) {
+		this.rentalAdd = rentalAdd;
+	}
+	public int getEa() {
+		return ea;
+	}
+	public void setEa(int ea) {
+		this.ea = ea;
+	}
+	public String getPhoto() {
+		return Photo;
+	}
+	public void setPhoto(String photo) {
+		Photo = photo;
+	}
+	public String getPosX() {
+		return posX;
+	}
+	public void setPosX(String posX) {
+		this.posX = posX;
+	}
+	public String getPosY() {
+		return posY;
+	}
+	public void setPosY(String posY) {
+		this.posY = posY;
+	}
+	public float getFee() {
+		return fee;
+	}
+	public void setFee(float fee) {
+		this.fee = fee;
+	}
+	public String getModelType() {
+		return modelType;
+	}
+	public void setModelType(String modelType) {
+		this.modelType = modelType;
+	}
+	public String getClassCode() {
+		return ClassCode;
+	}
+	public void setClassCode(String classCode) {
+		ClassCode = classCode;
+	}
+	public String getAreacode() {
+		return Areacode;
+	}
+	public void setAreacode(String areacode) {
+		Areacode = areacode;
+	}
+	
+}
